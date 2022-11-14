5f53715c395699316b68a042b8b8763de57170ff
Mario Josue Velasquez Banegas <MVelasquezB@ransa.net>
2022-11-14 17:08:29 -0600

    9no Commit

--- src/app/modules/finanzas/facturacion/facturacion.component.ts
+++ src/app/modules/finanzas/facturacion/facturacion.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataApi } from 'src/app/interfaces/dataApi';
 import { SharedService } from '../../shared/shared.service';
+import { FacturacionService } from '../facturacion.service';
 
 @Component({
   selector: 'app-facturacion',
@@ -8,9 +10,33 @@ import { SharedService } from '../../shared/shared.service';
 })
 export class FacturacionComponent implements OnInit {
   public dolares = [];
-  constructor( public sharedS : SharedService) { }
+  public parametros = [];
+  public fecha = new Date()
+  public espaciosBlancos = [1,2,3,4,5,6,7,8,9]
+  constructor( 
+    public sharedS      : SharedService,
+    public facturacionS : FacturacionService
+    ) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.cargarParametrosF()
+  }
+
+  cargarParametrosF(){
+    let url='seguridad/parametrosF';
+    let params = {
+      sede : 1
+    }
+  this.facturacionS.post(url,params).subscribe(
+    (res:DataApi | any )=>{
+      console.log(res)
+        if( !res?.hasError ){
+            this.parametros = res.data.Table0;
+            console.log( this.parametros )
+        }
+    }
+  )
+
   }
 
 
